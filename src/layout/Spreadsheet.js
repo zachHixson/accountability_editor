bdb1d8a208f8cb9580d6c6286e72d081d678b81a
Zach <zHixson.xbdi@gmail.com>
2020-05-18 18:19:10 -0500

    Added backend "studentList," which get's updated when the table does. Also added search feature

--- src/layout/Spreadsheet.js
+++ src/layout/Spreadsheet.js
@@ -7,17 +7,26 @@ const app = remote.app;
 const START_ROW = 2;
 
 let tempPath = path.join(app.getPath("appData"), "accountability_editor/tempSave.json");
+let studentList = [];
 
 class Student {
-    constructor(fName = '', lName = '', allergies = ''){
+    constructor(fName = '', lName = '', info = '', id = 0){
         this.fName = fName;
         this.lName = lName;
-        this.allergies = allergies;
+        this.info = info;
+        this.id = id;
+    }
+
+    getOutput(){
+        return {
+            fName : this.fName,
+            lName : this.lName,
+            allergies: this.info
+        }
     }
 }
 
 function createSpreadsheet(){
-    let table = document.getElementById("studentList");
     readTempList()
 }
 
@@ -28,8 +37,33 @@ function readTempList(){
             return;
         }
 
-        refreshList(JSON.parse(data));
+        loadJsonList(data)
+        refreshList(studentList);
     })
+}
+
+function loadJsonList(json){
+    let inputList = JSON.parse(json);
+    studentList = [];
+
+    for (let i = 0; i < inputList.length; i++){
+        studentList.push(new Student(
+            inputList[i].fName,
+            inputList[i].lName,
+            inputList[i].allergies,
+            i
+        ));
+    }
+}
+
+function listOutput(){
+    let outList = [];
+
+    for (let i = 0; i < studentList.length; i++){
+        outList.push(studentList[i].getOutput());
+    }
+
+    return JSON.stringify(outList);
 }
 
 function refreshList(nameList){
@@ -43,7 +77,8 @@ function refreshList(nameList){
         let row = createRow(
             nameList[i].fName,
             nameList[i].lName,
-            nameList[i].allergies
+            nameList[i].info,
+            nameList[i].id
         );
         table.appendChild(row);
     }
@@ -51,7 +86,7 @@ function refreshList(nameList){
     assignRowIds();
 }
 
-function createRow(fName = "", lName = "", info = ""){
+function createRow(fName = "", lName = "", info = "", id = 0){
     let newRow = document.createElement("tr");
     let fNameCell = newRow.insertCell(0);
     let fNameInp = document.createElement("input");
@@ -70,6 +105,29 @@ function createRow(fName = "", lName = "", info = ""){
     lNameCell.className = "nameField";
     deleteCell.className = "delete_cell";
 
+    newRow.setAttribute("studentRowID", id);
+    fNameInp.setAttribute("studentRowID", id);
+    lNameInp.setAttribute("studentRowID", id);
+    infoInp.setAttribute("studentRowID", id);
+
+    fNameInp.addEventListener("input", (event) => {
+        let elem = event.path[0];
+        let id = elem.getAttribute("studentRowID");
+        studentList[id].fName = elem.value;
+        saveTemp();
+    });
+    lNameInp.addEventListener("input", (event) => {
+        let elem = event.path[0];
+        let id = elem.getAttribute("studentRowID");
+        studentList[id].lName = elem.value;
+        saveTemp();
+    });
+    infoInp.addEventListener("input", (event) => {
+        let elem = event.path[0];
+        let id = elem.getAttribute("studentRowID");
+        studentList[id].info = elem.value;
+        saveTemp();
+    });
     deleteCell.addEventListener("click", deleteRow);
 
     fNameCell.appendChild(fNameInp)
@@ -89,7 +147,8 @@ function assignRowIds(){
 
 function insertRow(event){
     let table = document.getElementById("studentList");
-    table.appendChild(createRow());
+    studentList.push(new Student());
+    refreshList(studentList);
     assignRowIds();
     saveTemp();
 }
@@ -97,31 +156,15 @@ function insertRow(event){
 function deleteRow(event){
     let row = event.path[1]
     let rowID = row.getAttribute("rowID");
+    let studentRowID = row.getAttribute("studentRowID");
     document.getElementById("studentList").deleteRow(rowID);
+    studentList.splice(studentRowID, 1);
     assignRowIds();
     saveTemp();
 }
 
-function getTableData(){
-    let table = document.getElementById("studentList");
-    let students = [];
-
-    for (let i = START_ROW; i < table.rows.length; i++){
-        let childNodes = table.rows[i].childNodes;
-        students.push(new Student(
-            childNodes[0].firstChild.value,
-            childNodes[1].firstChild.value,
-            childNodes[2].firstChild.value
-        ));
-    }
-
-    return students;
-}
-
 function saveTemp(){
-    let data = getTableData();
-
-    fs.writeFile(tempPath, JSON.stringify(data), (err) => {
+    fs.writeFile(tempPath, listOutput(), (err) => {
         if (err){
             console.log(err);
             return;
@@ -130,8 +173,8 @@ function saveTemp(){
 }
 
 ipcRenderer.on('new_roster_opened', (event, data) => {
-    let list = JSON.parse(data);
-    refreshList(list);
+    loadJsonList(data)
+    refreshList(studentList);
     saveTemp();
 });
 
@@ -193,8 +236,7 @@ function sort(elem, conditionFunc){
     let curIndicator = elem.getElementsByClassName("sortIndicator")[0];
     let curSortDirection = curIndicator.getAttribute("sortOrder");
     let sortIndicators = document.getElementsByClassName("sortIndicator");
-    let list = getTableData();
-    list.sort(conditionFunc);
+    studentList.sort(conditionFunc);
 
     //set all sort indicators
     for (let i = 0; i < sortIndicators.length; i++){
@@ -205,12 +247,43 @@ function sort(elem, conditionFunc){
     if (curSortDirection === "desc"){
         curIndicator.innerHTML = "⯅";
         curIndicator.setAttribute("sortOrder", "asc");
-        list.reverse();
+        studentList.reverse();
     }
     else{
         curIndicator.innerHTML = "⯆";
         curIndicator.setAttribute("sortOrder", "desc");
     }
 
-    refreshList(list);
+    for (let i = 0; i < studentList.length; i++){
+        studentList[i].id = i;
+    }
+
+    refreshList(studentList);
+}
+
+function updateSearch(){
+    let search = document.getElementById("searchBox").value.toUpperCase();
+    let filteredList = studentList.filter((i) => {
+        return (
+            i.fName.toUpperCase().includes(search) || 
+            i.lName.toUpperCase().includes(search) ||
+            i.info.toUpperCase().includes(search)
+        )
+    });
+
+    if (search.length > 0){
+        let addBtn = document.getElementById("addName");
+        addBtn.hidden = true;
+    }
+    else{
+        let addBtn = document.getElementById("addName");
+        addBtn.hidden = false;
+    }
+    
+    refreshList(filteredList);
+}
+
+function clearSearch(){
+    document.getElementById("searchBox").value = "";
+    updateSearch();
 }
